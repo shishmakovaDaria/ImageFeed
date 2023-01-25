9a398d276d6a2a0dfcd2cc8bc79ddf25b26e270f
Shishmakova Daria <dary_shishka@mail.ru>
2023-01-25 21:30:17 +0300

    Добавлены UITabBarController и экран увеличения фотографии.

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,23 @@
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
