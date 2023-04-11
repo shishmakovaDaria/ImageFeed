e4a40ec61c3aa3c594548f79cf67843192ec5e8c
Shishmakova Daria <dary_shishka@mail.ru>
2023-04-11 18:36:59 +0300

    Добавлены тесты для WebView

---
+++ ImageFeedTests/WebViewPresenterSpy.swift
@@ -0,0 +1,19 @@
+import Foundation
+import ImageFeed
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var view: WebViewViewControllerProtocol?
+    var viewDidLoadCalled: Bool = false
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
